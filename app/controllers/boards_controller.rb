d0b033170424199dd4d99c340d21fca4fb1c746e
hiro <hiro.libe.0930@gmail.com>
2024-06-10 16:29:15 +0900

    無視したファイルの復元

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,4 @@
-class BoardsController < ApplicationController aaa
+class BoardsController < ApplicationController
   before_action :set_board, only: [:show]
   before_action :authenticate_user!, only: [:new, :create]#, :edit, :update, :destroy]
 
