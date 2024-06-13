65d6032c3d5f7ac4008eb2630ec95a2445de77b6
hiro <hiro.libe.0930@gmail.com>
2024-06-13 13:27:28 +0900

    AWS S3とRailsアプリの連携、環境変数の設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module TodoAppHiro
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+
+    Dotenv::Railtie.load
   end
 end
